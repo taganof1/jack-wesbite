4e34e616580f762a50dceb20da062eabe9fa81a3
taganof1 <jackfra72@gmail.com>
2023-09-22 11:48:13 +0100

    First Commit

---
+++ src/main/java/com/example/jackwesbite/JackWesbiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jackwesbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackWesbiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackWesbiteApplication.class, args);
+    }
+
+}
